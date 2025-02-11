04426e344a5f61a07b68e979af0569efc931d9e2
amy-j-lewis1432 <amyjlewis1432@gmail.com>
2025-02-11 22:47:14 +0000

    create and scan qr code
    can now create and scan qr codes

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),  # Keep home in main
+    path("qr", views.generate_qr, name="qr"),  # Keep home in main
     path("auth/", include("login.urls")),  # Move login/signup to login app
 ]
