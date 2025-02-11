e3ccb3661f64b06c1e92ad065cdf366b935a42e2
AndyThyerExeter <165232120+AndyThyerExeter@users.noreply.github.com>
2025-02-11 11:54:29 +0000

    Added Log in & Sign up

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("", include("django.contrib.auth.urls")),
+    path("signup", views.SignUpView.as_view(), name="signup"),
 ]
