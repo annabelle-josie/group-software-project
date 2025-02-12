46017a460abecdd06140b18fc6164ccd417b7d2a
annabelle-josie <ajroxford@gmail.com>
2025-02-12 14:34:11 +0000

    Pages added and added to nav

--- main/views.py
+++ main/views.py
@@ -5,3 +5,15 @@ from django.views.generic import CreateView
 
 def home(request):
     return render(request, "home.html")
+
+def leaderboard(request):
+    return render(request, "leaderboard.html")
+
+def garden(request):
+    return render(request, "garden.html")
+
+def events(request):
+    return render(request, "events.html")
+
+def market(request):
+    return render(request, "market.html")
