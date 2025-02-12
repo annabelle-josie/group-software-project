3473fc93bdc2ff81dd02e986f00e2998b754bd32
chishika <jason.grggg@gmail.com>
2025-02-12 14:31:17 +0000

    Create intial user model and gamekeepers

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'login',
+    'user_management'
 ]
 
 MIDDLEWARE = [
---
+++ user_management/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import UsersInfo
+
+admin.site.register(UsersInfo)
