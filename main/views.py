25d5a61dbd8fb4bc649fb694236b09293afc0d24
chishika <jason.grggg@gmail.com>
2025-02-16 19:46:52 +0000

    Nav bar fixed

--- main/views.py
+++ main/views.py
@@ -240,7 +240,7 @@ def add_purchased_plant(request):
         userStatObj = UserStats.objects.get(user_id=user.id)
         userLeaves = userStatObj.leaves
 
-        if(plant.price < userLeaves):
+        if(plant.price <= userLeaves):
             ownList = []
             for i in range(len(currentPlants)):
                 ownList.append(currentPlants[i])
@@ -249,7 +249,6 @@ def add_purchased_plant(request):
             user.owned_plants.set(ownList)
 
             newLeaves = userLeaves - plant.price
-            print(newLeaves)
             userStatObj.leaves = newLeaves
             userStatObj.save()
             return Response(status=status.HTTP_200_OK)
