7f7a5028d92c8d65cdaab15bfbbe138466704f52
David <dw689@exeter.ac.uk>
2025-02-21 16:03:12 +0000

    commenting for event management, garden, and login directories

--- garden/models.py
+++ garden/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+# Plant class is created to store the information of the plants
 class Plant(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     image = models.ImageField(upload_to="plant_images/")
     fact = models.TextField()
-    onMarket = models.BooleanField(default=True)
+    onMarket = models.BooleanField(default=True) # Boolean field to check if the plant is on market
 
     def __str__(self):
-        return self.name
+        return self.name # String representation of the plant
 
+# UserGarden class is created to store the plants that the user has in their garden
 class UserGarden(models.Model):
     user = models.OneToOneField("user_management.CustomUser", on_delete=models.CASCADE, primary_key=True)
     plant1Id = models.ForeignKey(Plant, on_delete=models.SET_NULL, null=True, blank=True, related_name="slot1")
