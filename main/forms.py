80ce045780007473aed197729e50d7234aac1722
amy-j-lewis1432 <amyjlewis1432@gmail.com>
2025-02-23 15:41:30 +0000

    delete forms

--- main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from . import models
-from django.forms import ModelForm
-
-class QRCodeForm(forms.Form):
-    text = forms.CharField(label='Text to Encode', max_length=255)
