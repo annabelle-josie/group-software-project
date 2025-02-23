6f57cfd5dd12b7d38f67402cd4eeddd9469cb217
OliverJones47 <146197749+OliverJones47@users.noreply.github.com>
2025-02-23 18:51:10 +0000

    more camelCase updates!!

--- garden/urls.py
+++ garden/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import gardenView, get_garden,update_garden
+from .views import gardenView, get_garden,updateGarden
 
 # urlpatterns is a list of URL patterns
 urlpatterns = [
     path("", gardenView, name="garden"),
     path("api/get_garden/<int:user_id>/", get_garden, name="get_garden"),
-    path("update_garden", update_garden, name="update_garden"),
+    path("updateGarden", updateGarden, name="updateGarden"),
     
 
 ]
