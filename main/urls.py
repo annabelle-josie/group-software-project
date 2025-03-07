f7fa04366d23f3a42f2c58b9fe413ef6583d1387
James <jamesbeasleycs@gmail.com>
2025-03-07 10:48:09 +0000

    Changed events functionality
    Added the ability to register to an event, instead of automatically being assigned to each event, in order to give users more control and improve database access speed. Also added the ability to let gamekeepers add event images.

--- main/urls.py
+++ main/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("market/api/add_purchased_plant", views.add_purchased_plant, name="add_purchased_plant"),  # Add purchased plant endpoint
     path('delete_event/<int:event_id>/', views.delete_event, name='delete_event'),
     path("profile/<str:username>/", views.profile),
+    path('sign_up_for_event/<int:event_id>/', views.sign_up_for_event, name='sign_up_for_event')
 ]
